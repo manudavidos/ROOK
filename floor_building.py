d3f5cfa098c78e8248abc0cb7d522d9a5ac06c84
Ani23ii <62055339+Ani23ii@users.noreply.github.com>
2020-03-14 22:57:49 +0400

    Update floor_building.py

--- floor_building.py
+++ floor_building.py
@@ -10,7 +10,7 @@ print(Paramaz_Avedissian_Building, "Paramaz Avedissian Building")
 print("==========")
 input_building = int(input())
 if(input_building == Main_Building):
-  print("Please select the floor: ")
+  print("You have chosen the Main Building. Please select the floor: ")
   while(Floor<=Main_Building_Floor):
     print(Floor)
     Floor = Floor+1
@@ -45,3 +45,30 @@ if(input_building == Main_Building):
               print("You have selected the sixth floor of the  Main Building!")
             else:
               print("ERR001: Input error")
+else:
+  if(input_building==Paramaz_Avedissian_Building):
+   print("You have chosen the Paramaz Avedissian Building. Please select the floor")
+   while(Floor<=Paramaz_Avedissian_Building_Floor):
+    print(Floor)
+    Floor = Floor+1
+  print("==========")
+  input_floor = int(input())
+
+  if(input_floor == 1):
+    print(" ")
+    print("You have selected the first floor of the Paramaz Avedissian Building!")
+  else:
+    if(input_floor == 2):
+      print(" ")
+      print("You have selected the second floor of the Paramaz Avedissian Building!")
+    else:
+      if(input_floor == 3):
+        print(" ")
+        print("You have selected the third floor of the Paramaz Avedissian Building!")
+      else:
+        if(input_floor == 4):
+          print(" ")
+          print("You have selected the fourth floor of the Paramaz Avedissian Building!")
+        else:
+            print("ERR001: Input error")
+              
