ee62585caf29fa51150ee25851eb917450821a89
Gaya-Amirjanyan <62055323+Gaya-Amirjanyan@users.noreply.github.com>
2020-03-21 17:19:24 +0400

    Update max_number_per_group.py

--- max_number_per_group.py
+++ max_number_per_group.py
@@ -1,24 +1,22 @@
-available = 0 
-booked = 1
-other = 2
-room_code = "003M"
-room_status = int(booked)
-room_location = "Main Building"
-room_capacity = 10
-room_occupied_seats = 0
-max_number_per_group = 5
-people_number = 0
-
-
-print("Room Code:", room_code)
-print("Room Location:", room_location)
-print("Number of Occupied Seats:", room_occupied_seats, "/", room_capacity)
+Room_Details = {
+  "Room Code:" : "003M",
+  "Room Status:" : "Available",
+  "Room Location:" : "Main Building",
+  "Room Capacity:" : 10,
+  "Room Occupied Seats:" : 0
+}
+People_number = {
+  "Number of People": 0,
+  "Maximum Number Per Group" : 5
+}
+for key in Room_Details.keys():
+  print(key, Room_Details[key])
 print("===========")
 print("Room booking details")
-
-people_number = int(input("Please enter the the number of people, the maximum number can be 5: "))
-if(people_number <= 5 ):
-  print("You have reserved the room for:", people_number)
+People_number["Number of People"] = int(input("Please enter the the number of people, the maximum number can be 5: "))
+if(People_number["Number of People"] <= 5 ):
+  
+  print("You have reserved the room for:", People_number["Number of People"])
 
 else:
   print("Sorry, you cannot have more than 5 people in your group")
