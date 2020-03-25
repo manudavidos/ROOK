c76bdd9992e6ed7e49437eaea22841b9e88d6a94
Gaya-Amirjanyan <62055323+Gaya-Amirjanyan@users.noreply.github.com>
2020-03-25 13:24:49 +0400

    Update booking_details.py

--- booking_details.py
+++ booking_details.py
@@ -1,59 +1,64 @@
-Room_Details = {
-  "Room Code:" : "003M",
-  "Room Status:" : "Available",
-  "Room Location:" : "Main Building",
-  "Room Capacity:" : 10,
-  "Room Occupied Seats:" : 0
+room_details = {
+  "room_code" : "003M",
+  "room_status" : "Available",
+  "room_location" : "Main Building",
+  "room_capacity" : 10,
+  "room_occupied_seats" : 0
 }
-People_number = {
-  "Number of People": 0,
-  "Maximum Number Per Group" : 5
+people_number = {
+  "number_of_people": 0,
+  "maximum_number_per_group" : 5
 }
-for key in Room_Details.keys():
-  print(key, Room_Details[key])
-print("===========")
-print("Room booking details")
-People_number["Number of People"] = int(input("Please enter the the number of people, the maximum number can be 5: "))
-if(People_number["Number of People"] <= 5 ):
-  
-  print("You have reserved the room for:", People_number["Number of People"])
 
-else:
-  print("Sorry, you cannot have more than 5 people in your group")
+print("Room Code:", room_details["room_code"])
+print("Room Location:", room_details["room_location"])
+print("Room Status:", room_details["room_status"])
+print("Room Capacity:", room_details["room_capacity"])
+print("Room Occupied Seats:", room_details["room_occupied_seats"])
+print("")
+print("Room booking details")
+
+tries = 0
+
+
+while(not(people_number["number_of_people"]<=5 and people_number["number_of_people"]>=1)):
+  if(tries!=0):
+    print("Sorry, you cannot have more than 5 people in your group")
+  people_number["number_of_people"] = int(input("Please enter the the number of people, the maximum number can be 5: "))
+  tries += 1
+print("You have reserved the room for:", people_number["number_of_people"])
   
 print()
 
-Allow_others_to_reserve = {
-    "allow others" : "yes",
-    "do not allow others" : "no"
+allow_others_to_reserve = {
+    "allow_others" : "yes",
+    "do_not_allow_others" : "no"
 }
-Allow_others_to_reserve["allow others"] =str(input("Do you want others to reserve the room as well? Answer yes/no: "))
-if(Allow_others_to_reserve["allow others"] == "yes"):
+allow_others_to_reserve["allow_others"] =str(input("Do you want others to reserve the room as well? Answer yes/no: "))
+if(allow_others_to_reserve["allow_others"] == "yes"):
     print("The room will be used only by your group. Others cannot reserve it")
-if(Allow_others_to_reserve["allow others"] == "no"):
+if(allow_others_to_reserve["do_not_allow_others"] == "no"):
     print("Others will be able to reserve your room")
-if(Allow_others_to_reserve["allow others"] != "yes"and Allow_others_to_reserve["allow others"] != "no"):
+if(allow_others_to_reserve["allow_others"] != "yes"and allow_others_to_reserve["do_not_allow_others"] != "no"):
     print("Error. Only yes/no answers are accepted")
 
 print()
 
-Hours_of_reservation = {
-    "First Option" :" 30 min",
-    "Second Option" : "1 hour",
-    "Third Option" : "1 hour and 30 min",
-    "Forth Option" : "2 hours"
-}
-print("How long do you want to reserve the room? We have four options")
-for key in Hours_of_reservation.keys():
-    print(key, ":", Hours_of_reservation[key])
-Hours_of_reservation = str(input("Please choose one of the options: "))
-if(Hours_of_reservation == "First Option"):
-    print("You reserved the room for 30 min")
-if(Hours_of_reservation == "Second Option"):
-    print("You reserved the room for 1 hour")
-if(Hours_of_reservation == "Third Option"):
-    print("You reserved the room for 1 hour and 30 min")
-if(Hours_of_reservation == "Fourth Option"):
-    print("You reserved the room for 2 hours")
-if(Hours_of_reservation != "First Option" and Hours_of_reservation != "Second Option" and Hours_of_reservation != "Third Option" and Hours_of_reservation != "Fourth Option"):
-    print("Error. Choose one of the options")
+hours_of_reservation = [30, 60, 90, 120]
+for option in range(len(hours_of_reservation)):
+    if(hours_of_reservation[option]<60):
+        print (str(hours_of_reservation[option])+" Minute(s)")
+    elif(hours_of_reservation[option]>=60):
+        print (str(hours_of_reservation[option]/60)+" Hour(s)")
+    
+hours_of_reservation= input(("How long do you want to reserve the room? We have " + str(len(hours_of_reservation)) + " options: "))
+if(hours_of_reservation=="30"):
+    print("You have reserved the room for: 30 Minutes")
+elif(hours_of_reservation=="1.0"):
+    print("You have reserved the room for: 1.0 Hour")
+elif(hours_of_reservation=="1.5"):
+    print("You have reserved the room for: 1.5 Hours")
+elif(hours_of_reservation=="2.0"):
+    print("You have reserved the room for: 2.0 Hours")
+else:
+    print("Error.")
