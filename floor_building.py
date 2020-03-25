350752a39fe1e7ecfb7eba3c080947c108a52f78
Ani23ii <62055339+Ani23ii@users.noreply.github.com>
2020-03-25 22:27:55 +0400

    Update floor_building.py

--- floor_building.py
+++ floor_building.py
@@ -1,97 +1,77 @@
-buildings = {
-  "main_building" : 10,
-  "paramaz_avedissian_building" : 20
-}
+import json
 
-floors = {
-  "MB_floors" : 6,
-  "PAB_floors" : 4
-}
-wings = {
-  "E" : "East",
-  "W" : "West"
+with open('data.json') as data_file:
+    data = json.load(data_file)
 
-}
-Floor = 0 #Ask the instructor if it is correct to write like this.
+floor = 0 #Ask the instructor if it is correct to write like this.
 
-print("Please select the building: ")
-print(buildings["main_building"], "Main Building")
-print(buildings["paramaz_avedissian_building"], "Paramaz Avedissian Building")
-print("==========")
-input_building = int(input())
-if(input_building != buildings["main_building"] and input_building!=buildings["paramaz_avedissian_building"]):
+print("Please select the building:")
+for building in data["location"]:
+    print(data["location"][building]["name"])
+print("=====")
+input_building = input("")
+if(input_building != data["location"]["main_building"]["name"] and input_building!=data["location"]["pab"]["name"]):
   print("Error: Input error")
-if(input_building == buildings["main_building"]):
+if(input_building == data["location"]["main_building"]["name"]): 
   print("You have chosen the Main Building. Please select the floor:")
-  while(Floor<= floors["MB_floors"]):
-    print(Floor)
-    Floor = Floor+1
-  print("==========")
+  while(floor<= data["location"]["main_building"]["number_of_floors"]):
+    print(floor)
+    floor = floor+1
+  print("=====")
   input_floor = int(input())
   if(input_floor<=6):
     if(input_floor == 0):
-      print(" ")
+      print("=====")
       print("You have selected the ground floor of the  Main Building!")
-    if(input_floor == 1):
+    elif(input_floor == 1):
       print(" ")
       print("You have selected the first floor of the  Main Building!")
-    else:
-      if(input_floor == 2):
+    elif(input_floor == 2):
         print(" ")
         print("You have selected the second floor of the  Main Building!")
-      else:
-        if(input_floor == 3):
-          print(" ")
-          print("You have selected the third floor of the  Main Building!")
-        else:
-          if(input_floor == 4):
-            print(" ")
-            print("You have selected the fourth floor of the  Main Building!")
-          else:
-            if(input_floor == 5):
-              print(" ")
-              print("You have selected the fifth floor of the  Main Building!")
-            else:
-              if(input_floor == 6):
-                print(" ")
-                print("You have selected the sixth floor of the  Main Building!")
+    elif(input_floor == 3):
+        print(" ")
+        print("You have selected the third floor of the  Main Building!")
+    elif(input_floor == 4):
+        print(" ")
+        print("You have selected the fourth floor of the  Main Building!")
+    elif(input_floor == 5):
+        print(" ")
+        print("You have selected the fifth floor of the  Main Building!")
+    elif(input_floor == 6):
+        print(" ")
+        print("You have selected the sixth floor of the  Main Building!")
   else:
     print("ERR001: Input error")
 
-
-
 else:
-  Floor=1
-  if(input_building == buildings["paramaz_avedissian_building"]):
+  floor=1
+  if(input_building == data["location"]["pab"]["name"]):
    print("You have chosen the Paramaz Avedissian Building. Please select the floor.")
-   while(Floor<=floors["PAB_floors"]):
-    print(Floor)
-    Floor = Floor+1
-  print("==========")
+   while(floor<=data["location"]["pab"]["number_of_floors"]):
+    print(floor)
+    floor = floor+1
+  print("=====")
   input_floor = int(input())
   
   if(input_floor == 1):
-
     print("You have selected the first floor of the Paramaz Avedissian Building!")
+  elif(input_floor == 2):
+    print(" ")
+    print("You have selected the second floor of the Paramaz Avedissian Building!")
+  elif(input_floor == 3):
+    print(" ")
+    print("You have selected the third floor of the Paramaz Avedissian Building!")
+  elif(input_floor == 4):
+    print(" ")
+    print("You have selected the fourth floor of the Paramaz Avedissian Building!")
   else:
-    if(input_floor == 2):
-      print(" ")
-      print("You have selected the second floor of the Paramaz Avedissian Building!")
-    else:
-      if(input_floor == 3):
-        print(" ")
-        print("You have selected the third floor of the Paramaz Avedissian Building!")
-      else:
-        if(input_floor == 4):
-          print(" ")
-          print("You have selected the fourth floor of the Paramaz Avedissian Building!")
-        else:
-            print("ERR00: Input error")
-  print("==========")
-  while(input_floor<=4):
-    print("Choose one of the wings: East/West")
+    print("ERR00: Input error")
+  print("=====")
+  while(input_floor<=4 and input_floor>=1):
+    print("Choose one of the wings: E/W")
     input_wing= str(input())
-    if(input_wing==wings["E"] or input_wing==wings["W"]):
+    if(input_wing==data["location"]["pab"]["code"][0] or input_wing==data["location"]["pab"]["code"][1]):
       print("You have chosen the",input_wing,"wing!")
       break
     else:
