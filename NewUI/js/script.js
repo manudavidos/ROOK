3f01b99427e30c9734fa9f6bee966f15af2f74d1
(David) Manvel Manvelyan <57355860+manudavidos@users.noreply.github.com>
2020-05-01 01:36:57 +0400

    Lots of UI improvements, new js functions
    Now there is a showerror("Error message") which solves the problem with User Communication

--- NewUI/js/script.js
+++ NewUI/js/script.js
@@ -7,8 +7,17 @@ var data = JSON.parse(localStorage.getItem('data'));
 //localStorage.setItem('data', JSON.stringify(data))
 //console.log(data.status.available.name)
 
+function showerror(errormessage){
+    document.getElementById("errormessagebox").style = "display:flex; height:32px;";
+    setTimeout(function(){ document.getElementById("errormessagebox").style = "display:flex; height:80px;"; }, 50);
+    document.getElementById("errormessagebox-content").innerHTML = errormessage;
+    setTimeout(function(){ document.getElementById("errormessagebox").style = "display:flex; height:32px;"; }, 3000);
+    setTimeout(function(){ document.getElementById("errormessagebox-content").innerHTML = ""; document.getElementById("errormessagebox").style = "display:flex; height:0px;"; }, 3050);
+    setTimeout(function(){ document.getElementById("errormessagebox").style = "display:none;"; }, 3400);
+}
+
 function showpopup(popupname) {
-    document.getElementById("unavailable").style = "display:inline";
+    document.getElementById("unavailable").style = "display:flex";
     document.getElementById("popups").style = "display:flex";
     document.getElementById(popupname).style = "display:flex";
     document.getElementById("unavailable").setAttribute( "onClick", `closepopup('${popupname}')` );
@@ -21,13 +30,38 @@ function closepopup(popupname) {
     document.getElementById("unavailable").setAttribute( "onClick", "" );
 }
 
-function gototheroom() {
-    document.getElementById("roominfo").style = "display:inline";
+function resetall() {
+    document.getElementById("roominfo").style = "display:none";
+    document.getElementById("roomlist").style = "display:none";
+    document.getElementById("popups").style = "display:none";
+    document.getElementById("unavailable").style = "display:none";
+}
+
+function findtheroom() {
+    document.getElementById("roominfo").style = "display:none";
+    document.getElementById("roomlist").style = "display:none";
+    document.getElementById("userroomnumber").style ="";
+    var userroomnumber = document.getElementById("userroomnumber").value;
+    if (data['rooms'][userroomnumber] != null){
+        document.getElementById("roominfo").style = "display:inline";
+        document.getElementById("roominfo").innerHTML += "<h1>" + data["rooms"][userroomnumber]["room_code"] +"</h1>";
+    } else {
+        showerror("Error: We could not find room with these details");
+    }
 }
     
 function login() {
     var useremail = document.getElementById("useremail").value;
     if (data['users'][useremail] != null){
         console.log(data['users'][useremail]['first_name'])
+    } else {
+        showerror("Error: No AUA Student with such credentials!");
     }
 }
+
+function allroomscount() {
+    var allroomscount = Object.keys(data.rooms).length;
+    document.getElementById("allroomscount").innerHTML = allroomscount;
+}
+
+window.onload = allroomscount;
